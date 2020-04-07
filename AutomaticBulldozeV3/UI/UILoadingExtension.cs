89efb781ac17d4835e0b63ee7eb8ce4b8fd72a84
John Rayes <live627@gmail.com>
2020-04-07 13:04:15 -0700

    the game will clean this up

--- AutomaticBulldozeV3/UI/UILoadingExtension.cs
+++ AutomaticBulldozeV3/UI/UILoadingExtension.cs
@@ -6,32 +6,14 @@ namespace AutomaticBulldozeV3.UI
 {
     public class UILoadingExtension : LoadingExtensionBase
     {
-        private LoadMode mode;
-        private UIAutoBulldozerPanel autoBulldozerPanel;
-
-        private void InitWindows()
-        {
-            var bulldozerBar = UIView.Find("BulldozerBar");
-            autoBulldozerPanel = bulldozerBar.AddUIComponent<UIAutoBulldozerPanel>();
-            autoBulldozerPanel.relativePosition = new Vector3(10.0f, -20.0f);
-        }
-
         public override void OnLevelLoaded(LoadMode mode)
         {
             if (mode != LoadMode.LoadGame && mode != LoadMode.NewGame)
                 return;
-            mode = mode;
 
-            InitWindows();
-        }
-
-        public override void OnLevelUnloading()
-        {
-            if (mode != LoadMode.LoadGame && mode != LoadMode.NewGame)
-                return;
-
-            if (autoBulldozerPanel != null)
-                Object.Destroy(autoBulldozerPanel);
+            UIComponent bulldozerBar = UIView.Find("BulldozerBar");
+            UIAutoBulldozerPanel autoBulldozerPanel = bulldozerBar.AddUIComponent<UIAutoBulldozerPanel>();
+            autoBulldozerPanel.relativePosition = new Vector3(10.0f, -20.0f);
         }
     }
 }
