534dcc68290a66400139b09e4b4440cdee14aea6
John Rayes <live627@gmail.com>
2020-04-07 17:06:48 -0700

    put version in title

--- AutomaticBulldozeV3/ModIdentity.cs
+++ AutomaticBulldozeV3/ModIdentity.cs
@@ -1,17 +1,16 @@
-﻿using System.Reflection;
-using ICities;
+﻿using ICities;
 
 namespace AutomaticBulldozeV3
 {
     public class ModIdentity : IUserMod
     {
-        public string Name => "Automatic Bulldoze v3";
+        public string Name => "Automatic Bulldoze (v{Assembly.GetExecutingAssembly().GetName().Version})";
 
         public string Description
         {
             get
             {
-                return $"Automatically destroys abandoned and burned buildings ({Assembly.GetExecutingAssembly().GetName().Version})"
+                return $"Automatically destroys abandoned and burned buildings"
 #if DEBUG
                         + " DEBUG version"
 #endif
