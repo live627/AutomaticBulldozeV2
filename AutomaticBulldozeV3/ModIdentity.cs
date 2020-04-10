ea8db61ce20bc001ac6aed66d75f67af658cb39a
John Rayes <live627@gmail.com>
2020-04-10 14:22:25 -0700

    interpolation the right string

--- AutomaticBulldozeV3/ModIdentity.cs
+++ AutomaticBulldozeV3/ModIdentity.cs
@@ -4,13 +4,13 @@ namespace AutomaticBulldozeV3
 {
     public class ModIdentity : IUserMod
     {
-        public string Name => "Automatic Bulldoze (v{Assembly.GetExecutingAssembly().GetName().Version})";
+        public string Name => $"Automatic Bulldoze (v{System.Reflection.Assembly.GetExecutingAssembly().GetName().Version})";
 
         public string Description
         {
             get
             {
-                return $"Automatically destroys abandoned and burned buildings"
+                return "Automatically destroys abandoned and burned buildings"
 #if DEBUG
                         + " DEBUG version"
 #endif
